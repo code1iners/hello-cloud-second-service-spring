382daf823302174762cd28989c7d040904a5434e
ethan <codeliner96@gmail.com>
2021-07-17 18:04:00 +0900

    Updated service server port by random value.

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,15 +1,22 @@
 package com.example.demo;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
 @RequestMapping(path = "/second-service")
-@Slf4j
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final Environment env;
 
     @GetMapping(path = "/hello")
     public String hello() {
@@ -23,7 +30,8 @@ public class HelloController {
     }
 
     @GetMapping(path = "/check")
-    public String check() {
-        return "Check 2";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Check 1 on PORT %s", env.getProperty("local.server.port"));
     }
 }
