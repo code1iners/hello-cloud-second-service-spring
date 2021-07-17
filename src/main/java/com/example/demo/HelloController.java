1d97ec7fc8ded2d8dfd5f14d768b0404a3044397
ethan <codeliner96@gmail.com>
2021-07-17 12:14:40 +0900

    Created a new project.

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/second-service")
+@Slf4j
+public class HelloController {
+
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "Hello 2";
+    }
+
+    @GetMapping(path = "messages")
+    public String messages(@RequestHeader("second-request") String header) {
+        log.info("header:{}",header);
+        return "Messages 2";
+    }
+}
